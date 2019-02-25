8806d765e0ddb0afbd7393454592293c4872f082
Niraj Aryal <niroz.aryal11@gmail.com>
2019-02-25 16:25:27 -0700

    Rewrites Card component

--- src/Card.js
+++ src/Card.js
@@ -1,63 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Card extends Component {
-    constructor(){
-        super()
-        this.state = {
-            clickedAnswer: '',
-            wrongAnswer: []
-        }
+  constructor() {
+    super();
+    this.state = {
+      clickedAnswer: ""
+    };
+  }
+  handleClick = event => {
+    const clickedAnswer = event.target.innerText;
+    const isCorrect = clickedAnswer === this.props.question.answer;
+    let wrongAnswers = JSON.parse(localStorage.getItem("wrong")) || [];
+    // debugger;
+    if (isCorrect) {
+      wrongAnswers = wrongAnswers.filter(answer => {
+        return answer.answer !== this.props.question.answer;
+      });
+      this.props.updateScore(); //come bakck to this
+    } else {
+      const hasAnswer = wrongAnswers.some(
+        answer => answer.answer === this.props.question.answer
+      );
+      if (!hasAnswer) {
+        wrongAnswers.push(this.props.question);
+      }
+    }
 
-    }
-    handleClick = (event) => {
-        
-        this.setState({
-          clickedAnswer: event.target.innerText
-        },() => {
-            if(this.state.clickedAnswer === this.props.scopeData.answer){
-            this.props.updateScore()} else
-            this.props.increment()
-        })
-    }
-    
-            
+    this.props.increment();
+    localStorage.setItem("wrong", JSON.stringify(wrongAnswers));
+    this.setState({
+      clickedAnswer: clickedAnswer
+    });
+  };
 
-            
-    render(){
-    const {clickedAnswer} = this.state
-    const answer = this.props.scopeData.answer
-    
-    
-    
-   
-    const options =  this.props.scopeData.options.map((option) => {
-        return (
-               <button onClick={this.handleClick}>{option}</button>
-            )
-        })
-    
-    
-        
-        
-        return(
-            <div className="hey">
-            <div className="proto-card">
-            <h1>{this.props.scopeData.question}</h1>
-            <div className="card-buttons">
-            {options}
-            </div>
-            </div>
-           
-            
-            </div>
-        
-        )
-    }
+  render() {
+    const { clickedAnswer } = this.state;
+    const answer = this.props.question.answer;
+
+    // console.log(this.props.question, "heyy");
+    const options = this.props.question.options.map(option => {
+      return <button onClick={this.handleClick}>{option}</button>;
+    });
+
+    return (
+      <div className="hey">
+        <div className="proto-card">
+          <h1>{this.props.question.question}</h1>
+          <div className="card-buttons">{options}</div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Card;
-                
-            
-            
-
-
-   
-    
