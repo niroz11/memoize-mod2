19755c317126139e16b4e22249448e9210b75c53
Niraj Aryal <niroz.aryal11@gmail.com>
2019-02-25 16:22:11 -0700

    Rewrites scopecomp again

--- src/Scopecomp.js
+++ src/Scopecomp.js
@@ -1,32 +1,42 @@
-import React, { Component } from 'react';
-import Card from './Card';
-import Instruction from './Instruction';
-import App from './App'
-
+import React, { Component } from "react";
+import Card from "./Card";
+import Instruction from "./Instruction";
 
 class Scopecomp extends Component {
-    
-    constructor(){
-        super()
-        this.state = {
-            clickedAnswer: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      clickedAnswer: ""
+    };
+  }
 
-    
-   
+  render() {
+    const { data, question, score } = this.props;
+    // console.log(this.props);
+    console.log(data, "this is data");
+    return (
+      <div className="proto">
+        {question === -1 && (
+          <Instruction
+            increment={this.props.increment}
+            toggleCard={this.props.toggleCard}
+            showCard={this.props.showCard}
+          />
+        )}
+        {question > -1 && question < data.length && (
+          <Card
+            question={data[question]}
+            increment={this.props.increment}
+            score={score}
+            updateScore={this.props.updateScore}
+            saveLocalStorage={this.props.localStorage}
+          />
+        )}
 
-
-    render(){
-        const {scopeData,scopeQuestion,score} = this.props
-        
-            
-        return (<div className="proto">
-            {scopeQuestion === -1 &&  <Instruction increment={this.props.increment}/>}
-            {scopeQuestion > -1 && scopeQuestion < 10 && <Card scopeData={scopeData[scopeQuestion]} increment={this.props.increment} score={score} updateScore={this.props.updateScore}/> }
-            {scopeQuestion === 10 && <p>{score}</p> && this.props.mainMenu() }
-        </div>)
-    }
+        {question > data.length - 1 && <p>{score}</p> && this.props.mainMenu()}
+      </div>
+    );
+  }
 }
 
 export default Scopecomp;
