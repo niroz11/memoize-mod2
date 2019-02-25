002c0ac3197631d36040d4742eac848f99b53a90
Niraj Aryal <niroz.aryal11@gmail.com>
2019-02-25 16:24:11 -0700

    Rewrites Instruction component

--- src/App.js
+++ src/App.js
@@ -1,125 +1,110 @@
-import React, { Component } from 'react';
-import prototypes from './Prototypedata'; 
-import scope from './Scopedata'; 
-import combo from './Combodata'; 
-import Scopecomp from './Scopecomp';
-import Combocomp from './Combocomp';
-import Protocomp from './Protocomp';
+import React, { Component } from "react";
+import Scopecomp from "./Scopecomp.js";
+
 // import './App.scss';
-
-
-
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      protoData : prototypes,
-      scopeData : scope,
-      comboData: combo,
+      data: [],
       score: 0,
-      clickValue:'',
-      scopeQuestion:-1,
-      protoQuestion:-1,
-      comboQuestion:-1,
-      scopeWrongAnswer:[]
-    }
+      clickValue: "",
+      question: -1,
+      showCard: true
+    };
   }
-      
-  handleClick = (event) => {
-    console.log(event.target.name, "heyy")
-    this.setState({
-      clickValue: event.target.name
-    })
-  }
-  
-  incrementScopeQuestion = () => {
-    let scopeQuestion = this.state.scopeQuestion
-    scopeQuestion++
-    
-    this.setState({
-      scopeQuestion
-    })
-  }
-  getMainMenu = () => {
-    
-    
-    this.setState({
-      clickValue:'',
-      scopeQuestion: -1,
-      protoQuestion:-1,
-      comboQuestion:-1
-
-    })
-  }
-  incrementProtoQuestion = () => {
-    let protoQuestion = this.state.protoQuestion
-    protoQuestion++
-    
-    this.setState({
-      protoQuestion
-    })
-  }
-  incrementComboQuestion = () => {
-    let comboQuestion = this.state.comboQuestion
-    comboQuestion++
-    
-    this.setState({
-      comboQuestion
-    })
-  }
-  updateScore = () => {
-    
-      this.setState({
-        score: this.state.score +10
+  componentDidMount() {
+    fetch("http://memoize-datasets.herokuapp.com/api/v1/nirajData")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          data: data.nirajData
+        });
       })
-    
+      .catch(error => {
+        throw new Error(error);
+      });
   }
-      
 
-  
+  handleClick = event => {
+    let question = this.state.question;
+    question++;
+    this.setState({
+      clickValue: event.target.name,
+      question
+    });
+  };
+
+  incrementQuestion = () => {
+    // debugger;
+    let newQuestion = this.state.question;
+    newQuestion++;
+
+    this.setState({
+      question: newQuestion
+    });
+  };
+  getMainMenu = () => {
+    this.setState({
+      clickValue: "",
+      question: -1
+    });
+  };
+
+  updateScore = () => {
+    this.setState({
+      score: this.state.score + 10
+    });
+  };
+  toggleCard = () => {
+    console.log("im working");
+    this.setState({
+      showCard: !this.state.showCard
+    });
+  };
+
   render() {
-    const { clickValue, protoData,scopeData,comboData,scopeQuestion,protoQuestion,comboQuestion  } = this.state;
-    // console.log(clickValue)
+    const incorrect = JSON.parse(localStorage.getItem("wrong")) || [];
+    // console.log(this.state.data);
+    const { clickValue, data } = this.state;
+
     const buttons = () => {
-      return(
-      <div className="buttons">
-        <button onClick={this.handleClick} name="scope">Scope</button>
-        <button onClick={this.handleClick} name="prototypes">Prototypes</button>
-        <button onClick={this.handleClick} name="combo">Combo</button>
-        </div>)
-    }
+      return (
+        <div className="buttons">
+          <button onClick={this.handleClick} name="scope">
+            Take the quiz
+          </button>
+          <button onClick={this.handleClick} name="redo">
+            Question Missed
+          </button>
+        </div>
+      );
+    };
     return (
-      
       <div className="entire-page">
-      <div className="welcome-page">
-        <h1 className="hello">Memoize</h1>
-        <p className="score">{this.state.score}</p>
-        
-        {clickValue === '' &&  buttons()}
-        <div className="question-section">
-        {clickValue === 'scope' && <Scopecomp scopeData={scopeData} 
-                                      scopeQuestion={scopeQuestion} 
-                                      increment={this.incrementScopeQuestion} 
-                                      score={this.state.score} 
-                                      updateScore={this.updateScore} 
-                                      mainMenu={this.getMainMenu} 
-                                      wrongAnswer={this.state.scopeWrongAnswer}
-                                   
-                                   />}
-        {clickValue === 'combo' && <Combocomp comboData={comboData} comboQuestion={comboQuestion} increment={this.incrementComboQuestion} score={this.state.score} updateScore={this.updateScore} mainMenu={this.getMainMenu}/>}
-        {clickValue === 'prototypes' && <Protocomp protoData={protoData} protoQuestion={protoQuestion} increment={this.incrementProtoQuestion} score={this.state.score} updateScore={this.updateScore} mainMenu={this.getMainMenu}/>}
+        <div className="welcome-page">
+          <h1 className="hello">Memoize</h1>
+          <p className="score">{this.state.score}</p>
+
+          {clickValue === "" && buttons()}
+          <div className="question-section">
+            <Scopecomp
+              data={clickValue === "scope" ? data : incorrect}
+              question={this.state.question}
+              increment={this.incrementQuestion}
+              score={this.state.score}
+              updateScore={this.updateScore}
+              mainMenu={this.getMainMenu}
+              localStorage={this.saveToLocalStorage}
+              toggleCard={this.toggleCard}
+              showCard={this.state.showCard}
+            />
+          </div>
         </div>
-        </div>
-        
-        
-            
       </div>
-    )
+    );
   }
 }
 
 export default App;
- 
-
-
--- src/Instruction.js
+++ src/Instruction.js
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Instruction extends Component {
-    
-    constructor(){
-        super()
-        this.state = {
-            clickedAnswer: '',
-            button: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      clickedAnswer: "",
+      button: ""
+    };
+  }
 
-    handleClick(event){
-        this.setState({
-            button: event.target.name
-        })
-    }
-    
+  handleClick(event) {
+    this.setState({
+      button: event.target.name
+    });
+  }
 
-
-    render(){
-        
-            
-        return (<div className="instruction">
-            <div className="inst-card">
+  render() {
+    if (this.props.showCard) {
+      return (
+        <div className="instruction">
+          <div className="inst-card">
             <div className="rules-text">
-            <p> * Play it cool! No cheating! :) </p>
-            <p> * Each question carries 10 points</p>
-            <p> * You may come back to solve answers you got it wrong</p>
-            
-            
-            <button onClick={this.props.increment}>Lets Start</button>
+              <p> * Play it cool! No cheating! :) </p>
+              <p> * Each question carries 10 points</p>
+              <p> * You may come back to solve answers you got it wrong</p>
+              <button onClick={this.props.toggleCard}>Lets Start</button>
             </div>
-            </div>
-        </div>)
+          </div>
+        </div>
+      );
+    } else {
+      return(
+          <div></div>
+      );
     }
+  }
 }
 
 export default Instruction;
