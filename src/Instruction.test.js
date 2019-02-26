433e8fb678be435f4b2bb12b8b13f7c26abbc98f
Niraj Aryal <niroz.aryal11@gmail.com>
2019-02-26 12:22:35 -0700

    Passes test for Instruction component

--- src/Instruction.test.js
+++ src/Instruction.test.js
@@ -6,27 +6,27 @@ let mockIncrement = jest.fn();
 let mockToggleCard = jest.fn();
 let mockShowCard = jest.fn();
 
-
 describe("Instruction", () => {
-    let wrapper;
-  
-    beforeEach(() => {
-      wrapper = shallow(
-        <Instruction 
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Instruction
         increment={mockIncrement}
         toggleCard={mockToggleCard}
         showCard={mockShowCard}
-        />
-      );
-    });
-  
-    it("should match the snapshot with all data passed in", () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-    // it("should invoke toggle method", () => {
-        
-    //     wrapper.find(".toggle-button").simulate("click");
-    //     expect(wrapper.instance().mockToggleCard().toHaveBeenCalled(1))
-    // })
-    
-  })
+      />
+    );
+  });
+
+  it("should match the snapshot with all data passed in", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should have a proper default state", () => {
+    expect(wrapper.state()).toEqual({ data: [], score: 0,clickValue: "",question: -1,showCard: true  });
+  });
+  it("should invoke toggle method", () => {
+    wrapper.find(".toggle-button").simulate("click");
+    expect(mockToggleCard).toHaveBeenCalled();
+  });
+});
